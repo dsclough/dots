e12d1bc32af928355761418281a4abd89a2a0755
root <root@vultr.guest>
2018-08-06 05:05:52 +0000

    added dependencies file

---
+++ dependencies.sh
@@ -0,0 +1,23 @@
+sudo apt-get install python3-pip
+sudo apt-get install libmysqlclient-dev
+sudo apt-get install mysql-server
+sudo apt-get install mysql-client
+sudo apt-get install python-dev
+sudo apt-get install python3-tkinter
+sudo apt-get install python3-pyqt5
+
+sudo pip3 install PyQt5
+sudo pip3 install qdarkstyle
+sudo pip3 install setuptools
+sudo pip3 install numpy
+sudo pip3 install pandas
+sudo pip3 install matplotlib
+sudo pip3 install mysql-connector
+sudo pip3 install dill
+sudo pip3 install PyQtChart
+sudo pip3 install lxml
+sudo pip3 install dropbox
+sudo pip3 install tqdm
+
+sudo apt-get install build-essential
+sudo apt-get install gdb
